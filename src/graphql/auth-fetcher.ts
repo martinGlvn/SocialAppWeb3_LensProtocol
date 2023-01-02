2941a7e8e98433cb35421ff41abed263b6e5524f
martinGlvn <lkn.galvan.martin@gmail.com>
2023-01-02 13:21:03 -0300

    Authenticated API request

--- src/graphql/auth-fetcher.ts
+++ src/graphql/auth-fetcher.ts
@@ -1,15 +1,42 @@
-// 
+import { isTokenExpired, readAccessToken } from "../lib/auth/helpers";
+import refreshAccessToken from "../lib/auth/refreshAccessToken";
+
+
 export const fetcher = <TData, TVariables>(
   query: string,
   variables?: TVariables,
   options?: RequestInit["headers"]
 ): (() => Promise<TData>) => {
+
+  async function getAccessToken() {
+    
+    const token = readAccessToken();
+
+    if (!token) return null;
+
+    let accessToken = token.accessToken;
+    
+    if (isTokenExpired(token.exp)) {
+      
+      const newToken = await refreshAccessToken();
+      if (!newToken) return null;
+      accessToken = newToken;
+    }
+
+    return accessToken;
+  }
+
+
   return async () => {
+    const token = typeof window !== "undefined" ? await getAccessToken() : null;
+
     const res = await fetch("https://api.lens.dev/", {
       method : "POST",
       headers :{
         "Content-Type" : "application/json",
         ...options,
+        "x-access-token": token ? token : "",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
         query,
