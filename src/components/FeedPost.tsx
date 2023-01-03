af4cc2b9e90f559abb340768352eb2f596c394e2
martinGlvn <lkn.galvan.martin@gmail.com>
2023-01-02 21:28:12 -0300

    Profile page

--- src/components/FeedPost.tsx
+++ src/components/FeedPost.tsx
@@ -43,7 +43,8 @@ export default function FeedPost({ publication }: Props) {
         </p>
 
         {/* Image / media of the post if there is one */}
-        {/* {(publication.metadata.image ||
+        
+        {(publication.metadata.image ||
           publication.metadata.media?.length > 0) && (
           <MediaRenderer
             src={
@@ -53,7 +54,7 @@ export default function FeedPost({ publication }: Props) {
             alt={publication.metadata.name || ""}
             className={styles.feedPostContentImage}
           />
-        )} */}
+        )}
       </div>
 
       <div className={styles.feedPostFooter}>
